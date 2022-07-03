f03bf85265202d329a01792a608943e321d1f472
arkadiuszma <arek.madejek@gmail.com>
2022-07-03 13:45:31 +0200

    First assertion test

---
+++ SeleniumBasics/AssertionTest.java
@@ -0,0 +1,41 @@
+package SeleniumBasics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+public class AssertionTest {
+    WebDriver driver;
+
+    @Test
+    public void test() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        WebElement accept = driver.findElement(By.xpath("//*[@id='L2AGLb']/div"));
+        accept.click();
+        WebElement search = driver.findElement(By.xpath("//input[@title='Szukaj']"));
+        search.click();
+        search.sendKeys("Selenium dev");
+        search.sendKeys(Keys.ENTER);
+        WebElement result = driver.findElement(By.xpath("//a[contains(@href,'selenium.dev')]//h3"));
+        Assert.assertTrue(result.isDisplayed());
+    }
+    @AfterTest
+        public void AfterTest() {
+
+                    driver.quit();
+
+
+    }
+}
+
+
+
+
